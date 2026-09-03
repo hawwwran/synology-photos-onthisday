0264cb4d9ebbdee0ce56cb701b0703cb59f9262a
hawwwran <github-release@hawwwran.com>
2026-09-03 13:11:25 +0200

    chore(release): v1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.hawwwran.photosonthisday"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         setProperty("archivesBaseName", "OnThisDay-$versionName")
     }
